e657f1aaa1840371c9531a8f3a2465ced2caf983
Houssamab54 <122807734+Houssamab54@users.noreply.github.com>
2023-09-09 13:57:24 +0200

    Add files via upload

---
+++ models/user.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines User class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
+
+
+class User(BaseModel, Base):
+    """Defines the class for the user
+    Attributes:
+        email: user email address
+        password: password for the user to login
+        first_name: first name for the user
+        last_name: last name for the user
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
